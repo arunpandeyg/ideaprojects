6909791180170bcea659995fd03ea42acc849c1f
Arun Pandey <128284476+arunpandeyg@users.noreply.github.com>
2026-08-02 18:57:23 +0530

    Add files via upload

---
+++ ExamPortalF/examPortal/src/app/pages/signup/signup.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  constructor(private userService: UserService, private snack: MatSnackBar) { }
+
+  public user = {
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    profile: '',
+  };
+
+  ngOnInit(): void { }
+
+  formSubmit() {
+    console.log(this.user);
+    if (this.user.username == '' || this.user.username == null) {
+      this.snack.open('User Name is required !!', 'ok', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right',
+      });
+
+      return;
+    }
+    //validation 
+    //adduser userService
+    this.userService.addUser(this.user).subscribe(
+      (data: any) => {
+        //success
+        console.log(data)
+        Swal.fire('Successfully done !!', 'User id is ' + data.userId, 'success');
+      },
+      (error) => {
+        //error
+        console.log(error)
+        this.snack.open('something went wrong!!', 'ok', {
+          duration: 3000,
+        }), [];
+      }
+    )
+    //for depricated subscribe()
+    // this.userService.addUser(this.user).subscribe(
+    //   {
+    //     next: (data) => console.log(data),
+
+    //     error: (err) => console.log(err),
+    //     complete: () => console.log("completed")
+    //   }
+    // )
+
+  }
+
+  //this.user can call here also
+
+
+
+
+}
